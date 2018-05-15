a1a939d2d77d8d9593b7fb990888ddd9b09682c9
Nirrvaa <mrbi220099@gmail.com>
2018-05-15 09:45:58 +0300

    fixed proptypes

--- src/components/presentationals/category/category.component.js
+++ src/components/presentationals/category/category.component.js
@@ -2,29 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Category = ({ active, count, children, onClick }) => {
-	if (active) {
-		return (
-			<div>
-				<button className='button button_active'>
-					{ children }
-				</button>
-				<span>
-					{ count }
-				</span>
-			</div>
-		);
-	}
-
 	return (
-		<button className='button' onClick={ onClick }>
-			{ children }
-		</button>
+		<div>
+			<button className={` button ${ active ? 'button_active' : '' }`}>
+				{ children }
+			</button>
+			<span>
+				{ count }
+			</span>
+		</div>
 	);
 };
 
 Category.propTypes = {
 	active: PropTypes.bool.isRequired,
-	count: PropTypes.numb.isRequired,
+	count: PropTypes.number.isRequired,
 	children: PropTypes.node.isRequired,
 	onClick: PropTypes.func.isRequired
 };
